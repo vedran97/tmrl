413014721e3633eac7dc16e2e12bcdf20c03f65d
VedantRanade <vedant@redaralabs.com>
2024-05-07 02:02:40 -0400

    Set dropout to false for this run

--- tmrl/custom/custom_models.py
+++ tmrl/custom/custom_models.py
@@ -42,7 +42,7 @@ def combined_shape(length, shape=None):
 def mlp(sizes, activation, output_activation=nn.Identity):
     layers = []
     dropout_prob=0.0
-    dropout_first_layer=True
+    dropout_first_layer=False
     for j in range(len(sizes) - 1):
         act = activation if j < len(sizes) - 2 else output_activation
         # layers += [nn.Linear(sizes[j], sizes[j + 1]), act()]
